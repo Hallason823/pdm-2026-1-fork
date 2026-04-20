0db181b9eab64cf1c9667dd2d57862e9d83ad561
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2026-04-20 01:57:27 +0000

    fix: update the backend

--- expo-01/api/index.js
+++ expo-01/api/index.js
@@ -1,26 +1,17 @@
 import axios from "axios";
 
-const urlBase = "https://aos-2026-1-s8ek.vercel.app";
-const headers = {
-  "X-Parse-Application-Id": "jnYc2snzzFZes5yr1TnS2l0ny7nB1Fyvlj8bq9wZ",
-  "X-Parse-JavaScript-Key": "iDYRd5O3Z2SnyfafWPneaqHiztKFSxFISMGqoBIY",
-};
+const urlBase = "https://aos-2026-1-s8ek.vercel.app/tarefas";
 const headersJson = {
-  ...headers,
   "Content-Type": "application/json",
 };
 
 export async function getTarefas() {
-  const response = await axios.get(urlBase, {
-    headers: headers,
-  });
-  return response.data.results;
+  const response = await axios.get(urlBase);
+  return response.data;
 }
 
 export async function getTarefa(id) {
-  const response = await axios.get(`${urlBase}/${id}`, {
-    headers: headers,
-  });
+  const response = await axios.get(`${urlBase}/${id}`);
   return response.data;
 }
 
@@ -33,7 +24,7 @@ export async function adicionarTarefa(novaTarefa) {
 
 export async function atualizarTarefa(tarefaAtualizada) {
   const response = await axios.put(
-    `${urlBase}/${tarefaAtualizada.id}`,
+    `${urlBase}/${tarefaAtualizada.objectId}`,
     tarefaAtualizada,
     {
       headers: headersJson,
@@ -43,8 +34,6 @@ export async function atualizarTarefa(tarefaAtualizada) {
 }
 
 export async function removerTarefa(id) {
-  const response = await axios.delete(`${urlBase}/${id}`, {
-    headers: headers,
-  });
+  const response = await axios.delete(`${urlBase}/${id}`);
   return response.data;
 }
